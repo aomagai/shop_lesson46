fcdf3926032ab3c04675f4964a945ff9695787bb
aomagai <aomagai@mail.ru>
2020-08-03 15:35:48 +0600

    Добавлены шаблоны

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -14,11 +14,9 @@ CATEGORY_CHOICES = (
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
-    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES,
-                                verbose_name='Категория')
+    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Категория')
     amount = models.IntegerField(verbose_name='Остаток', validators=(MinValueValidator(0),))
-    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2,
-                                validators=(MinValueValidator(0),))
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2, validators=(MinValueValidator(0),))
 
     def __str__(self):
         return f'{self.name} - {self.amount}'
